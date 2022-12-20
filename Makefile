e915ec9779f10ce04db1f48c93fe01320212e554
Lei Du <dulei66@gmail.com>
2022-12-20 15:57:01 -0800

    create initial project skeleton

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: lint, lint-check, test, run-local
+
+lint:
+	poetry run autoflake  --in-place --remove-all-unused-imports --verbose -r huma_signals tests
+	poetry run black huma_signals tests --target-version py38 -l120
+	poetry run flake8 huma_signals tests --max-line-length 120 --ignore "E203, W503" 
+	poetry run isort huma_signals tests
+
+lint-check:
+	poetry run black huma_signals tests --target-version py38 -l120 --check 
+	poetry run flake8 huma_signals tests --max-line-length 120 --ignore "E203, W503"
+	poetry run isort --check huma_signals tests 
+
+test:
+	ENV=test poetry run python3 -m pytest -v --color=yes
---
+++ huma_signals/settings.py
@@ -0,0 +1,41 @@
+import os
+from enum import Enum
+from pathlib import Path
+
+import dotenv
+import pydantic
+
+
+class Env(str, Enum):
+    DEVELOPMENT = "development"
+    TEST = "test"
+    STAGING = "staging"
+    TESTNET = "testnet"
+    PRODUCTION = "production"
+
+
+ENV = os.getenv("ENV")
+if ENV in (Env.PRODUCTION, Env.STAGING, Env.TESTNET):
+    env_path = Path(__file__).parent / "dotenv" / ".env"
+elif ENV == Env.TEST:
+    env_path = Path(__file__).parent / "dotenv" / "test.env"
+elif ENV == Env.DEVELOPMENT:
+    env_path = Path(__file__).parent / "dotenv" / "development.env"
+elif ENV is None:
+    raise ValueError("No ENV is defined")
+else:
+    raise ValueError(f"Unknown ENV: {ENV}")
+
+# Load environment variables from .env file
+# Note they won't override existing environment variables
+dotenv.load_dotenv(dotenv_path=env_path)
+
+
+class Settings(pydantic.BaseSettings):
+    class Config:
+        case_sensitive = False
+
+    env: str
+
+
+settings = Settings()
