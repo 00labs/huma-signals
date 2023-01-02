3e696e9bddec9fa8cb3491200d52fd0a8eb1967f
Lei Du <dulei66@gmail.com>
2023-01-02 10:33:34 -0800

    add wallet eth txn

---
+++ tests/adapters/wallet_eth_txns/test_adapter.py
@@ -0,0 +1,22 @@
+import pytest
+
+from huma_signals.adapters.wallet_eth_txns.adapter import WalletEthTransactionsAdapter
+
+
+@pytest.fixture(scope="session", autouse=True)
+def valid_address() -> str:
+    # vitalik.eth
+    return "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+
+
+def describe_wallet_eth_txns_adapter():
+    def it_works_e2e(valid_address):
+        result = WalletEthTransactionsAdapter.fetch(valid_address)
+        assert result is not None
+        assert result.total_transactions > 1400
+        assert result.total_sent > 900
+        assert result.total_received > 400
+        assert result.total_transactions == result.total_sent + result.total_received
+        assert result.wallet_teneur_in_days > 2600
+        assert result.total_income_90days > 0
+        assert result.total_transactions_90days > 0
