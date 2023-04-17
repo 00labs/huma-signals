6ea250f9960a2fc09107caf13114739e8b516b4c
Jiatu Liu <ljiatu@hotmail.com>
2023-04-17 14:15:53 -0700

    Capture network calls in unit tests

--- tests/adapters/allowlist/test_adapter.py
+++ tests/adapters/allowlist/test_adapter.py
@@ -1,6 +1,9 @@
 import pytest
 
 from huma_signals.adapters.allowlist import adapter
+from tests.helpers import vcr_helpers
+
+_FIXTURE_BASE_PATH = "/domain/adapters/allowlist"
 
 
 @pytest.fixture
@@ -14,13 +17,24 @@ def invalid_address() -> str:
 
 
 def describe_allowlist_adapter() -> None:
-    async def it_works_e2e(valid_address: str, invalid_address: str) -> None:
-        result = await adapter.AllowListAdapter().fetch(valid_address, "goerli")
-        assert result == adapter.AllowListSignal(on_allowlist=True)
-
-        result = await adapter.AllowListAdapter().fetch(invalid_address, "goerli")
-        assert result == adapter.AllowListSignal(on_allowlist=False)
-
     def it_validate_allowlist_endpoint() -> None:
         with pytest.raises(ValueError):
             adapter.AllowListAdapter(allowlist_endpoint="")
+
+    def when_the_address_is_valid() -> None:
+        async def it_returns_true(valid_address: str) -> None:
+            with vcr_helpers.use_cassette(
+                fixture_file_path=f"{_FIXTURE_BASE_PATH}/valid_address.yml"
+            ):
+                result = await adapter.AllowListAdapter().fetch(valid_address, "goerli")
+                assert result == adapter.AllowListSignal(on_allowlist=True)
+
+    def when_the_address_is_invalid() -> None:
+        async def it_returns_false(invalid_address: str) -> None:
+            with vcr_helpers.use_cassette(
+                fixture_file_path=f"{_FIXTURE_BASE_PATH}/invalid_address.yml"
+            ):
+                result = await adapter.AllowListAdapter().fetch(
+                    invalid_address, "goerli"
+                )
+                assert result == adapter.AllowListSignal(on_allowlist=False)
