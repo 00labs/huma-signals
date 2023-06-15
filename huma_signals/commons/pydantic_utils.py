94d8a50257b08ad4a41b4245c40826b0a3d8c35d
flowers-huang <flora221@stanford.edu>
2023-06-15 12:40:20 -0700

    Removed old util files in commons

--- huma_signals/commons/pydantic_utils.py
+++
@@ -1,8 +0,0 @@
-import pydantic
-from huma_utils import string_utils
-
-
-class CamelCaseAliased(pydantic.BaseModel):
-    class Config:
-        allow_population_by_field_name = True
-        alias_generator = string_utils.snake_to_camel
